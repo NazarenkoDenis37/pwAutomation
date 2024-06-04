bd1d4d86d2ef57b52f8cbfcae165398b89cde779
Denis Nazarenko <293728den@gmail.com>
2024-06-03 22:38:09 -0400

    todoPage

---
+++ tests/fixture/todo-page.ts
@@ -0,0 +1,21 @@
+import { test as base} from '@playwright/test';
+
+import {TodoPage} from './TodoPage';
+
+type Fixture = {
+    todoPage: TodoPage
+}
+
+export const test = base.extend<Fixture>({
+    todoPage: async ({page}, use) =>{
+        const todoPage = new TodoPage(page)
+        await todoPage.goto()
+        await todoPage.addTodo('Test Todo')
+        await todoPage.addTodo('Test Todo 2')
+        await todoPage.addTodo('Test Todo 3')
+        await use(todoPage)
+        await todoPage.removeAll()
+    }
+})
+
+export {expect} from '@playwright/test'
